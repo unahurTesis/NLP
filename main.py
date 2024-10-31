9e6ba3dcac567b72789b797b97f5601fa011fb70
Adrian <agyaniri@gmail.com>
2024-10-31 11:39:38 -0300

    Entrenamiento del modelo

--- main.py
+++ main.py
@@ -3,30 +3,7 @@ import pandas as pd
 from DibujarGrilla import DibujadorGrilla
 from SelectorDePuntosEnImagen import SelectorDePuntosEnImagen
 if __name__ == "__main__":
+    import tensorflow as tf
 
-    ruta_csv = 'datos/escala.csv'
-    directorio_imagenes = 'imagenes/grilla'
-    directorio_guardado = "imagenes/procesadas"
-    imagen = 'Cordon1_Seccion 1 horizontal C1.jpg'
+    print(tf.__version__)
 
-    datos = 'datos/escala.csv'
-
-    procesador = SelectorDePuntosEnImagen(
-        ruta_imagenes=directorio_imagenes,
-        archivo_csv=datos,
-        directorio_guardado=directorio_guardado,
-        columnas=8,
-        filas=5
-    )
-
-    matriz_manual = [
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0, 0, 1, 1]
-    ]
-
-    procesador.asignar_matriz_manual(imagen, matriz_manual)
-
-    #procesador.procesar_imagenes()
